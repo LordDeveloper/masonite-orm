136ad6d3ee02b1627b69e3fe968ca4fb9de9555c
lorddeveloper <losrkzpp@hi2.in>
2023-01-09 20:26:16 +0330

    Updated

--- src/masoniteorm/query/QueryBuilder.py
+++ src/masoniteorm/query/QueryBuilder.py
@@ -1441,6 +1441,12 @@ class QueryBuilder(ObservesEvents):
         if model:
             model.fill(updates)
             self.observe_events(model, "updating")
+            for attribute, value in model.__dirty_attributes__.items():
+                if updates.get(attribute, None) != value or value is None:
+                    if cast:
+                        value = model._set_casted_value(attribute, value)
+
+                    updates.update({attribute: value})
 
         self.new_connection().query(self.to_qmark(), self._bindings)
         if model:
